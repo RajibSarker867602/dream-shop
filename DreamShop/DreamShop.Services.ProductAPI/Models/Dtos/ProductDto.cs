aa8ffd8446b23006f95e748ce1a9304aa4114f0b
RajibSarker <Rajibsarker320@gmail.com>
2024-04-01 23:10:20 +0600

    product api project added with initial migration

---
+++ DreamShop/DreamShop.Services.ProductAPI/Models/Dtos/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DreamShop.Services.ProductAPI.Models.Dtos
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ DreamShop/DreamShop.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
+
+namespace DreamShop.Services.ProductAPI.Models
+{
+    public class Product
+    {
+        [Key]
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        [Range(1, 1000)]
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
